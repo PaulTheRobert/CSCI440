e2f6e02a9400d5c62df84b341a360f5496adec64
Paul Davis <pauldavis1826@gmail.com>
2022-09-08 17:50:55 -0700

    trying to make this back to hello world

--- numpagefaults.c
+++ numpagefaults.c
@@ -1,30 +1,24 @@
-#include <linux/module.h>    // included for all kernel modules
-#include <linux/kernel.h>    // included for KERN_INFO
-#include <linux/init.h>      // included for __init and __exit macros
+#include <linux/module.h>	// included for all kernel modules
+#include <linux/kernel.h>	// included for KERN_INFO
+#include <linux/init.h>	  // included for __init and __exit macros
+
 #include <linux/seq_file.h>
-#include <linux/vmstat.h>   // 
+#include <linux/vmstat.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("PaulRobertDavis");
 MODULE_DESCRIPTION("A simple numpagefaults module");
 
-unsigned long countPageFault = 0;
-
-
 static int __init numpagefaults_init(void)
 {
-    // printk(KERN_INFO "Hello world!\n");
-    // proc_file_entry = proc_create("numpagefaults", 0, NULL, &ct_file_ops);
-
-    printk("%u", countPageFault);
-    
+    printk(KERN_INFO "Hello world!\n");
     return 0;    // Non-zero return means that the module couldn't be loaded.
 }
 
 static void __exit numpagefaults_init(void)
 {
-    printk(KERN_INFO "Cleaning up module.\n");
-    remove_proc_entry("numpagefaults", NULL);
+	printk(KERN_INFO "Cleaning up module.\n");
+	remove_proc_entry("numpagefaults", NULL)
 }
 
 module_init(numpagefaults_init);
